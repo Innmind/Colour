10ca77b61e714588f692daf17ded43746eec484a
peter279k <peter279k@gmail.com>
2020-09-02 01:30:23 +0800

    Fix namespace for ColourTest class

--- tests/ColourTest.php
+++ tests/ColourTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types = 1);
 
-namespace Tests\Innmind\Colour\Colour;
+namespace Tests\Innmind\Colour;
 
 use Innmind\Colour\{
     Colour,
--- tests/Fixtures/ColourTest.php
+++ tests/Fixtures/ColourTest.php
@@ -6,9 +6,10 @@ namespace Tests\Innmind\Colour\Fixtures;
 use Fixtures\Innmind\Colour\Colour;
 use Innmind\Colour\RGBA;
 use Innmind\BlackBox\Set;
+use Innmind\BlackBox\Random;
 use PHPUnit\Framework\TestCase;
 
-class ColourTest extends TestCase
+class ColourTest extends TestCase implements Random
 {
     public function testInterface()
     {
@@ -16,10 +17,15 @@ class ColourTest extends TestCase
 
         $this->assertInstanceOf(Set::class, $set);
 
-        foreach ($set->values() as $value) {
+        foreach ($set->values(new self) as $value) {
             $this->assertInstanceOf(Set\Value::class, $value);
             $this->assertTrue($value->isImmutable());
             $this->assertInstanceOf(RGBA::class, $value->unwrap());
         }
+    }
+
+    public function __invoke(int $min, int $max): int
+    {
+        return random_int($min, $max);
     }
 }
